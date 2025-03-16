aed91d956e62f4dd83d1949f833c3a6c115260a8
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-16 20:30:02 +0600

    Setup the react router dom and tailwind css wit daisyui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <section>
+      <h2 className="text-5xl text-red-700">Monk.Ar</h2>
+      <button className="btn btn-neutral">More Videos</button>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-800">Here is main</div>,
+  },
+]);
+
+export default router;
